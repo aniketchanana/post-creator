8b181cddd13558556d86bbaa03581c16ed219f4c
Aniket <aniketchanana@gmail.com>
2023-08-26 23:56:31 +0530

    Handeled some events

--- post-creator/creator.js
+++ post-creator/creator.js
@@ -1,17 +1,30 @@
 const mainContainer = document.getElementById("mainContainer");
-
+let selectedTextElement = null;
+const focusOutOfSelectedTextElement = () => {
+  if (selectedTextElement) {
+    selectedTextElement.classList.remove("textContainerBoxGuide");
+    selectedTextElement.setAttribute("contenteditable", false);
+    window.parent.postMessage(
+      // TODO: catch in react application and set the state
+      { type: "ELEMENT_SELECTED", data: null },
+      "*"
+    );
+  }
+};
+window.addEventListener("blur", () => {
+  focusOutOfSelectedTextElement();
+});
+mainContainer.addEventListener("click", (e) => {
+  if (e.target.id === "mainContainer") {
+    focusOutOfSelectedTextElement();
+  }
+});
 const ELEMENT_TYPE = {
   BACKGROUND: "BACKGROUND",
   TEXT: "TEXT",
 };
 
 const elementsPool = {};
-
-const createNewDiv = (elementId) => {
-  const divElement = document.createElement("div");
-  divElement.setAttribute("id", elementId);
-  return divElement;
-};
 
 const createStyleStr = (styleObject) => {
   let styleStr = "";
@@ -25,26 +38,71 @@ const applyStylesToBg = (styleData) => {
   mainContainer.setAttribute("style", createStyleStr(styleData));
 };
 
-const getAlignmentGuideBox = (elementId) => {
-  const guideBox = document.createElement("div");
-  guideBox.setAttribute("id", `guide##${elementId}`);
-  guideBox.setAttribute("class", "guide");
-  // const overlayClickHandler = document.createElement("div");
-  // overlayClickHandler.setAttribute("class", "overlayClickHandler");
-  // overlayClickHandler.setAttribute("id", `guideOverlay##${elementId}`);
-  return guideBox;
-};
-
 const createTextBoxAndAddToMain = (elementId, styleData) => {
   if (!elementsPool[elementId]) {
-    // const alignmentGuideBox = getAlignmentGuideBox(elementId);
-
     const textContainer = document.createElement("div");
     textContainer.setAttribute("contenteditable", false);
     textContainer.setAttribute("id", elementId);
-    textContainer.setAttribute("class", "guide");
+    textContainer.classList.add("textContainerBox");
 
-    // alignmentGuideBox.appendChild(textContainer);
+    textContainer.addEventListener("click", () => {
+      selectedTextElement = textContainer;
+      textContainer.classList.add("textContainerBoxGuide");
+      window.parent.postMessage(
+        // TODO: catch in react application and set the state
+        { type: "ELEMENT_SELECTED", data: elementId },
+        "*"
+      );
+    });
+
+    textContainer.addEventListener("keydown", (e) => {
+      window.parent.postMessage(
+        // TODO: catch in react application and set the state
+        {
+          type: "ELEMENT_TEXT_UPDATED",
+          data: { elementId, text: e.target.innerText },
+        },
+        "*"
+      );
+    });
+
+    textContainer.addEventListener("paste", (e) => {
+      e.preventDefault();
+
+      const text = e.clipboardData.getData("text/plain");
+      document.execCommand("insertText", false, text);
+      window.parent.postMessage(
+        // TODO: catch in react application and set the state
+        {
+          type: "ELEMENT_TEXT_UPDATED",
+          data: { elementId, text },
+        },
+        "*"
+      );
+    });
+
+    textContainer.addEventListener("dblclick", () => {
+      textContainer.setAttribute("contenteditable", true);
+      const range = document.createRange();
+      const sel = window.getSelection();
+
+      range.selectNodeContents(textContainer);
+      range.collapse(false);
+
+      sel.removeAllRanges();
+      sel.addRange(range);
+    });
+
+    textContainer.addEventListener("blur", () => {
+      textContainer.classList.remove("textContainerBoxGuide");
+      textContainer.setAttribute("contenteditable", false);
+      window.parent.postMessage(
+        // TODO: catch in react application and set the state
+        { type: "ELEMENT_SELECTED", data: null },
+        "*"
+      );
+    });
+
     mainContainer.appendChild(textContainer);
     window.parent.postMessage({ type: "ELEMENT_CREATED" }, "*");
 
